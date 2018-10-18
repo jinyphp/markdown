9d15ea0b1cbe9c5eb928a509066fa04ab42927b4
hojinlee <infohojin@gmail.com>
2018-10-18 19:02:58 +0900

    init

---
+++ src/Markdown.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the jinyPHP package.
+ *
+ * (c) hojinlee <infohojin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Jiny\Markdown;
+
+class Markdown
+{
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+}
