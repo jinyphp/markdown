c577d6ece4a29b706a0609b5b44e7f73fa51a435
hojinlee <infohojin@gmail.com>
2024-05-13 18:40:18 +0900

    for laravel component

---
+++ src/JinyMarkdownServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Jiny\Markdown;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\Support\Facades\File;
+use Livewire\Livewire;
+
+class JinyMarkdownServiceProvider extends ServiceProvider
+{
+    private $package = "jiny-markdown";
+    public function boot()
+    {
+        // 모듈: 라우트 설정
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', $this->package);
+
+        // 데이터베이스
+        $this->loadMigrationsFrom(__DIR__.'/../databases/migrations');
+
+
+    }
+
+
+
+    public function register()
+    {
+        /* 라이브와이어 컴포넌트 등록 */
+        $this->app->afterResolving(BladeCompiler::class, function () {
+
+        });
+    }
+}
